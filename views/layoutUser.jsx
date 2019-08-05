5f4b9472fd7b0b0638c73e96e8d18a353d1ef21c
caspianzx <caspianzx@gmail.com>
2019-08-05 11:33:50 +0800

    updated new user dashboard and navbar

--- views/layoutUser.jsx
+++ views/layoutUser.jsx
@@ -4,42 +4,58 @@ var React = require('react');
 class Layout extends React.Component {
   render() {
     return (
-      <html>
-        <head>
-            <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous"/>
+        <html>
+            <head>
+                <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous"/>
 
-            <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-            <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
-            <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+                <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+                <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+                <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
 
-            <link rel="stylesheet" href="/style.css"></link>
-        </head>
-        <body>
+                <link rel="stylesheet" href="/style.css"></link>
+            </head>
 
-          <nav class="navbar navbar-expand-lg navbar-light bg-light ">
-            <a class="navbar-brand " href="/user/events">Logo: Let's Meet Up!!</a>
-            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-             </button>
+            <body>
+                <nav className="navbar navbar-expand-lg navbar-light bg-light ">
+                    <a className="navbar-brand " href="/user/events">Logo: Let's Meet Up!!</a>
+                    <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                        <span className="navbar-toggler-icon"></span>
+                    </button>
 
-        <div class="collapse navbar-collapse justify-content-end" id="navbarSupportedContent">
-            <ul class="navbar-nav ">
-                <li class="nav-item">
-                    <a class="nav-link" href="/user/events">Browse Events<span class="sr-only"></span></a>
-                </li>
-                <li className="nav-item">
-                    <a className="nav-link" href="/user" id="navName">{this.props.name}<span className="sr-only"></span></a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="/logout">Logout<span class="sr-only"></span></a>
-                </li>
-            </ul>
-    </div>
-    </nav>
-            {this.props.children}
 
-        </body>
-      </html>
+                    <div className="collapse navbar-collapse justify-content-end" id="navbarSupportedContent">
+
+
+                        <ul className="navbar-nav ">
+                        <li class="nav-item dropdown">
+                            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Manage your events
+                             </a>
+                            <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                                <a class="dropdown-item" href="#">Action</a>
+                                <a class="dropdown-item" href="#">Another action</a>
+
+                            </div>
+                        </li>
+
+
+
+
+
+                            <li className="nav-item">
+                                <a className="nav-link" href="/user/events">Browse Events<span className="sr-only"></span></a>
+                            </li>
+                            <li className="nav-item">
+                                <a className="nav-link" href="/user" id="navName">{this.props.name}<span className="sr-only"></span></a>
+                            </li>
+                            <li className="nav-item">
+                                <a className="nav-link" href="/logout">Logout<span className="sr-only"></span></a>
+                            </li>
+                        </ul>
+                    </div>
+                </nav>
+                {this.props.children}
+            </body>
+        </html>
     );
   }
 }
--- views/user.jsx
+++ views/user.jsx
@@ -42,6 +42,7 @@ class Form extends React.Component {
 
         return (
             <Layout name={this.props.name} >
+
                 <div className="container">
                     <div className="row">
                         <div className="col-6 offset-3">
@@ -50,36 +51,24 @@ class Form extends React.Component {
                             <br/>
                         </div>
                     </div>
-                    <div className="row">
-                        <div className="col-6">
-                            <div className ="container p-0">
-                                <div className="row">
-                                    <div className="col">
-                                        <p className ="text-center" id="attending"> Events that you are attending!</p>
-                                        <ul className="list-group">
-                                                {eventRegisteredDisplay}
-                                        </ul>
 
-                                    </div>
-                                </div>
-                                <div className="row">
-                                    <div className="col">
-                                        <p id="hosting"> Events that you are hosting!</p>
-                                            <ul className="list-group">
-                                                {eventList}
-                                            </ul>
-                                    </div>
-                                </div>
-                            </div>
-                    </div>
-                        <div className="col-6">
-                            <p> Hosting an event?</p>
-                            <p>Click the button below to get started!</p>
-                            <a className="btn btn-secondary" href="/events/new">Get started</a>
+                    <div className="row">
+                        <div className="col-6 event-attend" >
+                            <p className ="text-center" id="attending"> Events that you are attending!</p>
+                            <ul className="list-group">
+                                    {eventRegisteredDisplay}
+                            </ul>
+                        </div>
+
+                        <div className="col-6 event-host">
+                            <p id="hosting"> Events that you are hosting!</p>
+                            <ul className="list-group">
+                                {eventList}
+                            </ul>
                         </div>
                     </div>
-
                 </div>
+
             </Layout>
         );
     }
