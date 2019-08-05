b7192bd7b40b55070aa937d4d73c97a5efccb265
caspianzx <caspianzx@gmail.com>
2019-08-05 16:55:17 +0800

    updated new layout for all pages.

--- views/layoutRegistration.jsx
+++ views/layoutRegistration.jsx
@@ -11,20 +11,21 @@ class Layout extends React.Component {
             <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
             <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
             <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+            <link rel="stylesheet" href="/bootstrap.min.css"></link>
 
             <link rel="stylesheet" href="/style.css"></link>
         </head>
         <body>
-            <nav class="navbar navbar-expand-lg navbar-light bg-light ">
-                <a class="navbar-brand " href="/">Logo: Let's Meet Up!!</a>
-                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                    <span class="navbar-toggler-icon"></span>
+            <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
+                <a className="navbar-brand " href="/">Eventix</a>
+                <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon"></span>
                 </button>
 
-                <div class="collapse navbar-collapse justify-content-end" id="navbarSupportedContent">
-                    <ul class="navbar-nav ">
-                        <li class="nav-item">
-                            <a class="nav-link" href="#">Help<span class="sr-only"></span></a>
+                <div className="collapse navbar-collapse justify-content-end" id="navbarSupportedContent">
+                    <ul className="navbar-nav ">
+                        <li className="nav-item">
+                            <a className="nav-link" href="#">Help<span className="sr-only"></span></a>
                         </li>
                     </ul>
                 </div>
--- views/layoutUser.jsx
+++ views/layoutUser.jsx
@@ -12,12 +12,13 @@ class Layout extends React.Component {
                 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
                 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
 
+                <link rel="stylesheet" href="/bootstrap.min.css"></link>
                 <link rel="stylesheet" href="/style.css"></link>
             </head>
 
             <body>
-                <nav className="navbar navbar-expand-lg navbar-light bg-light ">
-                    <a className="navbar-brand " href="/user/events">Logo: Let's Meet Up!!</a>
+                <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
+                    <a className="navbar-brand " href="/user/events">Eventix</a>
                     <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                         <span className="navbar-toggler-icon"></span>
                     </button>
